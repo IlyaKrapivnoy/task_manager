a90180045299a511753cdafa63076298e2a60e15
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-07-28 11:28:59 +0300

    change tasks woth useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
     const title = 'Task Tracker';
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Text Hannah',
+            day: 'Nov 12 at 1:35pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Call Matt',
+            day: 'Nov 13 at 3:10pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Shopping',
+            day: 'Nov 15 at 12:00pm',
+            reminder: false,
+        },
+    ])
 
   return (
     <div className="container">
         <Header title={title} />
-        <Tasks />
+        <Tasks tasks={tasks} />
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,10 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Text Hannah',
-        day: 'Nov 12 at 1:35pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Call Matt',
-        day: 'Nov 13 at 3:10pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Shopping',
-        day: 'Nov 15 at 12:00pm',
-        reminder: false,
-    },
-]
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
+   
+
     return (
         <>
-            {tasks.map((task) => (<h3>{ task.text }</h3>))}
+            {tasks.map((task) => (<h3 key={task.id}>{ task.text }</h3>))}
         </>
     )
 }
