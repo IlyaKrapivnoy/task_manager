04e7a738dc8fc9bff8678948face1b894fd5d616
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-08-31 00:57:06 +0300

    implement add new task function

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ function App() {
     ]);
 
     // Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 1000) + 1;
+        const newTask = { id, ...task };
+        setTasks([...tasks, newTask]);
+    };
 
     // Delete Task
     const deleteTask = (id) => {
@@ -45,7 +50,7 @@ function App() {
     return (
         <div className='container'>
             <Header title={title} />
-            <AddTask />
+            <AddTask onAdd={addTask} />
             {tasks.length > 0 ? (
                 <Tasks
                     tasks={tasks}
